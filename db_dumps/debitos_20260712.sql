2e090b4208402a37a2e8ca158081b631de86d6df
Adelmo Filho <adelmotop10@gmail.com>
2026-07-12 03:30:01 +0100

    backup auto 20260712

--- db_dumps/debitos_20260712.sql
+++ db_dumps/debitos_20260712.sql
@@ -1,11 +1,3 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE debitos_carregamento(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        driver_id INTEGER, driver_nome TEXT, license_plate TEXT,
-        charger_id INTEGER, charger_nome TEXT,
-        kwh REAL, preco_kwh REAL, valor REAL,
-        inicio TEXT, fim TEXT, semana TEXT,
-        auto INTEGER DEFAULT 0,
-        criado_em TEXT DEFAULT CURRENT_TIMESTAMP);
 COMMIT;
